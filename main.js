979976e59d7849ea0e61c489a8a75cddb9004846
abdelrahman-seada <abdelrahmanseada7@gmail.com>
2025-02-16 00:44:44 +0200

    modified main.js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-const formatDate = (date = new Date()) => {
-    const days = date.getDate();
-    const months = date.getMonth()+1 ;
-    const years = date.getFullYear() ;
-    return `${days}/${months}/${years}`;
-}
-
-console.log(formatDate())
+const x = 10;
